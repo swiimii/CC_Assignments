429d06c295971ee7880e2af2b15798c6f7c01750
Samuel Scherer <swiimii962@gmail.com>
2021-09-15 17:52:06 -0400

    Project 1 work in progress

--- Project_1/flaskapp.py
+++ Project_1/flaskapp.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/update_details")
 def hello_world():
-    return render_template('hello.html')
+    return render_template('basic_details.html')
+
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+    # do stuff
+    data = "Data Updated! \n" \
+        f"New first name: {request.form['fname']}\n" \
+        f"New last name: {request.form['lname']}"
+    return data
