f41da6c97e7e08d261b6b9a6195a2cb20d6fa715
Samuel Scherer <swiimii962@gmail.com>
2021-09-19 12:18:20 -0400

    omg it was trying to run with python2

--- Project_1/flaskapp/flaskapp.py
+++ Project_1/flaskapp/flaskapp.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@application.route("/update_details")
+@app.route("/update_details")
 def hello_world():
     return render_template('basic_details.html')
 
-@application.route('/submit_data', methods=['POST'])
+@app.route('/submit_data', methods=['POST'])
 def submit_data():
     # do stuff
     data = "Data Updated! \n" \
