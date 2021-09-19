4dd27e33f557f876879d6ec12df94b4ab98f93cd
Samuel Scherer <swiimii962@gmail.com>
2021-09-19 12:06:12 -0400

    rework folder layout

--- Project_1/flaskapp/app.py
+++ Project_1/flaskapp/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/update_details")
+@application.route("/update_details")
 def hello_world():
     return render_template('basic_details.html')
 
-@app.route('/submit_data', methods=['POST'])
+@application.route('/submit_data', methods=['POST'])
 def submit_data():
     # do stuff
     data = "Data Updated! \n" \
